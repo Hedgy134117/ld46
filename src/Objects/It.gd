46d89eea8b1576fa68b64f2d1bf4826b2143fe4e
Hedgy134117 <hedgy134117@gmail.com>
2020-04-18 14:27:40 -0400

    bug fixed more or less, and health bar

--- src/Objects/It.gd
+++ src/Objects/It.gd
@@ -1,8 +1,8 @@
 extends RichTextLabel
 
 
-
+var healthToLose = 10
 
 func _on_Area2D_area_entered(area: Area2D) -> void:
-	print(area.get_parent().text)
 	area.get_parent().queue_free()
+	get_node("/root/Control/HealthBar").value -= healthToLose
